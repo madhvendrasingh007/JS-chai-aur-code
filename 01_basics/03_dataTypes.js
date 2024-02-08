5dea8385d9f84c36b1a6cdc2819f181c3593e3ed
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2024-02-08 19:09:18 +0000

    added dataTypes

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,29 @@
+// Numbers:
+let length = 16;
+
+// Strings:
+let color = "Yellow";
+
+// Booleans
+let x = true;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// null => standalone value / it is not undefined but it is empty
+let num = null;
+
+// undefined => declared the variable but didn't assigned the value to it
+let state;
+
+// symbol => unique
+
+
+console.log(person)
+console.table([length, color, x, num, state])
+
+console.log(typeof "madhvendra")
+console.log(typeof length)
+console.log(typeof person)
+console.log(typeof null)    // object type
+console.log(typeof undefined)   // undefined type
