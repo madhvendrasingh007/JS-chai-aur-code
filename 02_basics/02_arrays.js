1087835c56200ce3229e1f6f43403e46d4a2f924
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2024-09-28 15:05:47 +0000

    Updation in code

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,28 +1,32 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros);  //  [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
 // console.log(marvel_heros[3][1]);    //  flash
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);   //  ['thor', 'Ironman','spiderman',[ 'superman', 'flash', 'batman' ],'superman','flash','batman']
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);   //  [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ] only if line for is removed
 
 // stread operator
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
-// console.log(all_new_heros);
+console.log(all_new_heros); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 // nested array to single array
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+console.log(real_another_array);    /*[
+                                        1, 2, 3, 4, 5,
+                                        6, 7, 6, 7, 4,
+                                        5
+                                      ]*/
 
 
 
-console.log(Array.isArray("Madhvendra"))
+console.log(Array.isArray("Madhvendra"))    // false
 console.log(Array.from("Madhvendra"))
 console.log(Array.from({name: "madhvendra"})) // interesting    o/p : []
 
